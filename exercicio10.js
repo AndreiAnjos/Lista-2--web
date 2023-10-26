cc409bf1f41b02bf09425e252e5a5ed08e061a70
AndreiAnjos <147105537+AndreiAnjos@users.noreply.github.com>
2023-10-26 19:04:50 -0300

    Update exercicio10.js

--- exercicio10.js
+++ exercicio10.js
@@ -1,7 +1,7 @@
 let numero = prompt('insira seu salario');
-if (numero >= 2112) {
+if (numero >= 25344) {
   console.log('Você devera pagar imposto de renda');
-} else if (numero < 2112) {
+} else if (numero < 25344) {
   console.log('você não precisa pagar imposto de renda ');
 }
 else {
